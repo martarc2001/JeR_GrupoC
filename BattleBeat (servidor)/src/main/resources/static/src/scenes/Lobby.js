73fa4087e2942d2581b4da3d83a1e3de596d3c9c
martarc2001 <martarc2001@hotmail.com>
2021-11-20 18:34:12 +0100

    Actualización Menú y Lobby
    Corrección errores en Menú, Lobyy ahora contiene un cuadro de texto interactuable

--- BattleBeat (servidor)/src/main/resources/static/src/scenes/Lobby.js
+++ BattleBeat (servidor)/src/main/resources/static/src/scenes/Lobby.js
@@ -2,6 +2,7 @@ import { anchoJuego, altoJuego } from "../init.js";
 import Tutorial from "./Tutorial.js";
 
 
+
 export default class Lobby extends Phaser.Scene {
     constructor() {
         super({
@@ -14,11 +15,18 @@ export default class Lobby extends Phaser.Scene {
     preload() {
         this.load.image('Fondo1', '/src/images/Fondo1.png');
         this.load.image('boton', '/src/images/Menu/BOTON_Jugar.png');
-        this.load.image('filtroColor', '/src/images/Menu/Filtro Color.png')
+        this.load.image('filtroColor', '/src/images/Menu/Filtro Color.png');
+
+        this.load.html('miCajaTexto', '/src/scenes/cajaTexto.html');
+        
+        this.load.path = './sounds/';
+        this.load.audio('musicmenu', 'harmonica.mp3');
+        this.load.audio('djsound', 'DJscratch.mp3');
     }
 
     create(){
-
+        this.djsound = this.sound.add('djsound', { volume: 0.2 });
+        this.cameras.main.fadeFrom(1000, 57, 47, 236);
         this.fondo1 = this.add.image(0, 0, 'Fondo1');
         this.fondo1.setOrigin(0, 0);
         this.fondo1.setScale(anchoJuego / this.fondo1.width, altoJuego / this.fondo1.height); //Imagen se escalará con el resto del juego 
@@ -32,7 +40,7 @@ export default class Lobby extends Phaser.Scene {
         var escalaBotones=6.5;
 
         //Botón para comenzar la partida (Versión offline juego--fase 2)        
-        this.botonJugar = this.add.image(anchoJuego / 2, altoJuego / 10, 'boton');
+        this.botonJugar = this.add.image(anchoJuego *9/ 10, altoJuego*9/ 10, 'boton');
         this.botonJugar.setScale(anchoJuego / (this.botonJugar.width * escalaBotones), altoJuego / (this.botonJugar.height * escalaBotones));
         this.botonJugar.setInteractive();//Para que funcionen los eventos
 
@@ -47,11 +55,39 @@ export default class Lobby extends Phaser.Scene {
 
         //Función para clic del botón y cambio de escena
         this.botonJugar.on('pointerdown', function (event) {
-        
+            this.djsound.play()
             this.scene.add("miTutorial", new Tutorial);
             this.scene.start("miTutorial"); //Inicializa tutorial de partida creada al hacer clic, elimina esta escena de menú
             this.scene.remove();
         }, this);
+
+
+
+        this.cuadroTexto = this.add.dom(anchoJuego/2,altoJuego*9/10).createFromCache("miCajaTexto");
+        this.cuadroTexto.setOrigin(0.5, 0.5);
+        this.cuadroTexto.setScale(anchoJuego/this.botonJugar.width, altoJuego / this.botonJugar.height )
+        this.cuadroTexto.setPosition((anchoJuego/2.5),altoJuego*9/10);
+
+
+        this.pantalla = this.add.text(640, 250, "Hello, --", {
+            color: "#FFFFFF",
+            fontSize: 60,
+            fontStyle: "bold"
+        }).setOrigin(0.5);
+    
+        this.enviarMensaje = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    
+        this.enviarMensaje.on("down", event => {
+            let textoAEnviar = this.cuadroTexto.getChildByName("texto");
+            if(textoAEnviar.value != "") {
+                this.pantalla.setText("Hello, " + textoAEnviar.value);
+                textoAEnviar.value = "";
+            }
+        });
+
+
+
+
     }
 
     update(){
